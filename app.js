03687d3abe5f6b760689ec091480ea6d6ecefab0
minhtoannguyenit <toanminh.nguyenit@gmail.com>
2024-05-20 15:07:00 -0500

    adding api to get all music and user deatail

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const userRoute = require("./routes/userRoute");
+const cors = require("cors");
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/songs", require("./routes/songRoute"));
 app.use("/playlist", require("./routes/playlistRoute"));
@@ -11,6 +13,11 @@ app.use('/users', userRoute);
 app.get("/", (req, res) => {
   res.json("Ok");
 });
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message });
+});
+
 app.listen(PORT, () => {
   console.log("Server is running on PORT:" + PORT);
 });
---
+++ public/scripts/script.js
@@ -0,0 +1,33 @@
+// Initiate the script
+window.onload = function() {
+  console.log('Script loaded');
+  login();
+}
+
+function login() {
+  // login function
+  document.getElementById('loginForm').addEventListener('click', function(e) {
+    e.preventDefault();
+    console.log('login');
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    fetch('http://localhost:3000/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email, password })
+    }).then(response => response.json())
+      .then(data => {
+        // console.log(data);
+        if (data.error) {
+          alert(data.error);
+        } else {
+          alert('Login successful', data.data);
+          localStorage.setItem("userId", data.data.id);
+          localStorage.setItem("token", data.data.token);
+          window.location.href = '/views/dashboard.html';
+        }
+      });
+  });
+}
